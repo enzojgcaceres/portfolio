cdeeadb8c0579669024806bbe7899bc3eb907698
enzojgcaceres <enzojgcaceres@gmail.com>
2024-08-26 18:57:44 -0300

    dotenv added for mailing

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -16,12 +16,12 @@ const ContactPage = () => {
     setSuccess(false);
 
     emailjs
-      .sendForm(
-        process.env.NEXT_PUBLIC_SERVICE_ID,
-        process.env.NEXT_PUBLIC_TEMPLATE_ID,
-        form.current,
-        process.env.NEXT_PUBLIC_PUBLIC_KEY
-      )
+  .sendForm(
+    process.env.NEXT_PUBLIC_SERVICE_ID,
+    process.env.NEXT_PUBLIC_TEMPLATE_ID,
+    form.current,
+    process.env.NEXT_PUBLIC_PUBLIC_KEY
+  )
       .then(
         () => {
           setSuccess(true);
